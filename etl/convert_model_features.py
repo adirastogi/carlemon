6c28902e377dbdd0d9f9538cb14b5946ef9d0816
Aditya Rastogi <arastog2@illinois.com>
2014-11-28 20:27:44 -0600

    Modified training and test data to replace model and submodel features

--- etl/convert_model_features.py
+++ etl/convert_model_features.py
@@ -7,14 +7,22 @@ def convert_features(filename,attr_table,attr_idx):
         for line in filename:
             line = line.strip().split(',');
             if line[0]=='RefId':
-                new_line = line[:attr_idx] + attr_table + line[attr_idx+1:];
+                new_line = line[:attr_idx] + attr_table + ['others'] +  line[attr_idx+1:];
             else:
                 new_line = line[:attr_idx];
                 attr_value = line[attr_idx];
                 attr_tokens = set(attr_value.split());
+                found = False;
                 for token in attr_table:
-                    if token in attr_tokens: new_line += ['yes'];
-                    else: new_line += ['no'];
+                    if token in attr_tokens: 
+                        found = True;
+                        new_line += ['yes'];
+                    else: 
+                        new_line += ['no'];
+                if found:
+                    new_line += ['no'] 
+                else :
+                    new_line += ['yes']
                 new_line += line[attr_idx+1:]
             print ','.join(new_line);
                 
